f183f14bae7612ebf2ec5cae901ae86c9a7447ed
Trung <trung@example.com>
2026-07-05 18:56:26 +0700

    update OrderServiceImpl logic and add method in repository

--- src/main/java/com/coffeeshopmanagement/repository/OrderRepository.java
+++ src/main/java/com/coffeeshopmanagement/repository/OrderRepository.java
@@ -3,5 +3,9 @@ package com.coffeeshopmanagement.repository;
 import com.coffeeshopmanagement.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    Order findByDiningTableIdAndStatus(Long tableId, String status);
+    List<Order> findByStatus(String status);
 }
--- src/main/java/com/coffeeshopmanagement/service/OrderServiceImpl.java
+++ src/main/java/com/coffeeshopmanagement/service/OrderServiceImpl.java
@@ -1,56 +1,154 @@
 package com.coffeeshopmanagement.service;
 
-import com.coffeeshopmanagement.entity.Order;
-import com.coffeeshopmanagement.entity.Product;
-import com.coffeeshopmanagement.repository.OrderRepository;
+import com.coffeeshopmanagement.entity.*;
+import com.coffeeshopmanagement.repository.*;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Transactional
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final DiningTableRepository diningTableRepository;
+    private final AccountRepository accountRepository;
+    private final ProductRepository productRepository;
+    private final OrderDetailRepository orderDetailRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    public OrderServiceImpl(ProductRepository productRepository, OrderRepository orderRepository, DiningTableRepository diningTableRepository, AccountRepository accountRepository, OrderDetailRepository orderDetailRepository) {
+        this.diningTableRepository = diningTableRepository;
+        this.productRepository = productRepository;
+        this.accountRepository = accountRepository;
         this.orderRepository = orderRepository;
+        this.orderDetailRepository = orderDetailRepository;
     }
 
     @Override
     public Order createOrder(Long tableId, Long accountId) {
+        DiningTable diningTable = diningTableRepository.findById(tableId).orElse(null);
+
+        if (diningTable == null) {
+            throw new EntityNotFoundException("DiningTable not found");
+        }
+
+        Account account = accountRepository.findById(accountId).orElse(null);
+        if (account == null) {
+            throw new EntityNotFoundException("Account not found");
+        }
+
+        if (orderRepository.findByDiningTableIdAndStatus(tableId, "OPEN") != null) {
+            throw new IllegalStateException("Order already exists");
+        }
+        Order order = new Order();
+        order.setAccount(account);
+        order.setDiningTable(diningTable);
+        order.setStatus("OPEN");
+        order.setTotalAmount(BigDecimal.ZERO);
+        order.setCreatedAt(LocalDateTime.now());
+        return orderRepository.save(order);
     }
 
     @Override
     public void addItemToOrder(Long orderId, Long productId, Integer quantity) {
-        Order order = orderRepository
+        Order order = orderRepository.findById(orderId).orElse(null);
+        if (order == null) {
+            throw new EntityNotFoundException("Order not found");
+        }
+        Product product = productRepository.findById(productId).orElse(null);
+        if (product == null) {
+            throw new EntityNotFoundException("Product not found");
+        }
+
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Quantity must be greater than 0");
+        }
+
+        OrderDetail detail = orderDetailRepository.findByOrderIdAndProductId(orderId, productId);
+        if (detail == null) {
+            OrderDetail orderDetail = new OrderDetail();
+            orderDetail.setOrder(order);
+            orderDetail.setProduct(product);
+            orderDetail.setQuantity(quantity);
+            orderDetail.setPriceAtOrder(BigDecimal.valueOf(product.getPrice()));
+            orderDetailRepository.save(orderDetail);
+        } else {
+            detail.setQuantity(detail.getQuantity() + quantity);
+            orderDetailRepository.save(detail);
+        }
+        order.setTotalAmount(calculateOrderTotal(orderId));
+        orderRepository.save(order);
     }
 
     @Override
     public void updateItemQuantity(Long orderDetailId, Integer newQuantity) {
-        Order order = orderRepository.getOne(orderDetailId);
+        OrderDetail detail = orderDetailRepository.findById(orderDetailId)
+                .orElseThrow(() ->  new EntityNotFoundException("OrderDetail not found"));
+
+        if(newQuantity <= 0){
+            throw new IllegalArgumentException("Quantity must be greater than 0");
+        }
+
+        detail.setQuantity(newQuantity);
+        orderDetailRepository.save(detail);
+        Order order = detail.getOrder();
+        order.setTotalAmount(calculateOrderTotal(order.getId()));
+        orderRepository.save(order);
     }
 
     @Override
     public void removeItemFromOrder(Long orderDetailId) {
+        OrderDetail detail = orderDetailRepository.findById(orderDetailId)
+                .orElseThrow(() ->  new EntityNotFoundException("OrderDetail not found"));
 
+        Order order = detail.getOrder();
+        orderDetailRepository.delete(detail);
+        order.setTotalAmount(calculateOrderTotal(order.getId()));
+        orderRepository.save(order);
     }
 
     @Override
     public Order getOrderById(Long orderId) {
-        return null;
+        return orderRepository.findById(orderId)
+                .orElseThrow(() ->  new EntityNotFoundException("Order not found"));
     }
 
     @Override
     public List<Order> getOpenOrders() {
-        return List.of();
+        return orderRepository.findByStatus("OPEN");
     }
 
     @Override
     public Order getOpenOrderByTable(Long tableId) {
-        return null;
+        diningTableRepository.findById(tableId)
+                .orElseThrow(() -> new EntityNotFoundException("DiningTable not found"));
+
+        Order order = orderRepository.findByDiningTableIdAndStatus(tableId, "OPEN");
+
+        if (order == null) {
+            throw new EntityNotFoundException("Open order not found");
+        }
+
+        return order;
     }
 
     @Override
     public BigDecimal calculateOrderTotal(Long orderId) {
-        return null;
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new EntityNotFoundException("Order not found"));
+
+        BigDecimal total = BigDecimal.ZERO;
+
+        for (OrderDetail detail : orderDetailRepository.findByOrderId(orderId)) {
+
+            BigDecimal subTotal = detail.getPriceAtOrder()
+                    .multiply(BigDecimal.valueOf(detail.getQuantity()));
+
+            total = total.add(subTotal);
+        }
+
+        return total;
     }
 }
